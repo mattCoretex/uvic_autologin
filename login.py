fece513daf027dde72bfbd305ce6bfc0655ade6f
Matt O <mek.arnuparp@gmail.com>
2019-10-04 17:12:44 -0700

    Update login.py

--- login.py
+++ login.py
@@ -1,40 +1,41 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import sys
 
 from scrapy.http import FormRequest
-
-# netlinkid = sys.argv[1]
-# password = sys.argv[2]
 
 class LoginSpider(scrapy.Spider):
     name = 'uvic'
     allowed_domains = ['uvic.ca']
     start_urls = ['https://www.uvic.ca/']
+    coursespaces_url = ['https://coursespaces.uvic.ca/']
 
     def parse(self, response):
         login_page = response.xpath('//*[@class="glbl__lnk glbl__lnk--signin glbl__lnk--icon glbl__lnk--unauth"]/@href').extract_first()
-        absolute_next_page_url = response.urljoin(login_page)
-        return scrapy.FormRequest.from_response(
-            response,
-            formdata={'username': username, 'password': password}
-        )
-        # yield scrapy.Request(absolute_next_page_url)
-    	# formdata={'csrf_token': csrf_token,
-    	# 			'username': 'foo',
-    	# 			'password': 'bar'}, callback=self.parse_after_login)
+        absolute_login_page_url = response.urljoin(login_page)
+        yield FormRequest(absolute_login_page_url,
+            formdata={ 'username': 'foo', 'password': 'bar'}, callback=self.parse_page2)
 
-        # yield FormRequest('absolute_next_page_url')
-    	# csrf_token = response.xpath('//*[@name="csrf_token"]/@value').extract_first()
-     #    yield FormRequest('http://quotes.toscrape.com/login', 
+    def parse_page2(self, response):
+        coursespaces_url = 'https://coursespaces.uvic.ca/'
+        yield FormRequest(coursespaces_url)
+
 
     def parse_after_login(self, response):
-    	if response.xpath('//a[text()="Logout"]')
+    	if response.xpath('//a[text()="Logout"]'):
     		self.log("You logged in")
 
+    # def parse_cs(self, response, start_urls):
+    #   yield dict(
+    #     main_url=s,
+    #     other_url=response.url,
+    #     foo=foo,
+    # )
 
-
-
-
-
-
+"""
+def parse_page2(self, response, main_url, foo):
+    yield dict(
+        main_url=main_url,
+        other_url=response.url,
+        foo=foo,
+    )
+"""
