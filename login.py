d8f2db3bd50061171aa0c2c9ca324a8af0bcf392
Mek O <mek.arnuparp@gmail.com>
2020-01-01 17:07:20 -0800

    Update login through UVIC

--- login.py
+++ login.py
@@ -1,63 +1,33 @@
-# -*- coding: utf-8 -*-
-
-"""
- scrapy crawl myspider -a category=electronics -a domain=system
-class MySpider(scrapy.Spider):
-    name = 'myspider'
-
-    def __init__(self, category='', **kwargs):
-        self.start_urls = [f'http://www.example.com/{category}']  # py36
-        super().__init__(**kwargs)  # python3
-
-    def parse(self, response)
-        self.log(self.domain)  # system
-
-"""
-
-"""
- response.xpath('//*[@class="login"]').extract_first()
-
-"""
-
-
 import scrapy
-
 from getpass import *
 from scrapy.http import FormRequest
+from scrapy.http import Request
 from scrapy.shell import inspect_response
 
-usr = input("NetLinkID: ")
-pwd = getpass('Password: ')
 
 class LoginSpider(scrapy.Spider):
-    name = 'uvic'
-    allowed_domains = ['uvic.ca']
-    start_urls = ['https://www.uvic.ca/']
-    coursespaces_url = ['https://coursespaces.uvic.ca/']
+    name = 'coursespaces'
+    allowed_domains = ['www.uvic.ca']
+    start_urls = ['https://www.uvic.ca/cas/login?service=https%3A%2F%2Fcoursespaces.uvic.ca%2Flogin%2Findex.php']
 
+    #UVIC CourseSpaces login
     def parse(self, response):
-        login_page = response.xpath('//*[@class="glbl__lnk glbl__lnk--signin glbl__lnk--icon glbl__lnk--unauth"]/@href').extract_first()
-        absolute_login_page_url = response.urljoin(login_page)
-        yield FormRequest(absolute_login_page_url,
-            formdata={ 'username': usr, 'password': pwd}, callback=self.parse_coursespaces)
+        usr = input("NetLinkID: ")
+        pwd = getpass('Password: ')
+        form = {'username':usr,'password':pwd}
+        return FormRequest.from_response(response, formid='fm1', formdata=form, callback=self.parse_coursespaces)
 
+    #get links to each active courses
     def parse_coursespaces(self, response):
-        coursespaces_url = 'https://coursespaces.uvic.ca/'
-        yield FormRequest(coursespaces_url, callback=self.parse_inspection)
+        # link = response.xpath("//*[@class=' row card-deck']/div[@class='card mb-3 courses-view-course-item']/a/@href").extract()
+        # - last 3 for irrelevent links
+        link = response.xpath("//*[@class=' row card-deck']/div[@class='card mb-3 courses-view-course-item']/a/@href").extract()
+        req = Request(link[0], callback=self.parse_inspection)
+        print("\n\n\n\n\n\n\n%s\n\n\n\n\n\n" % req.url)
+        inspect_response(response,self)
 
     def parse_inspection(self, response):
-        inspect_response(response, self)
-
-
-
-    def parse_after_login(self, response):
-    	if response.xpath('//a[text()="Logout"]'):
-    		self.log("You logged in")
-
-
-
-
-
+        inspect_response(response,self)
 
 
 
