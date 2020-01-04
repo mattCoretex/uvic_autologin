1ca37ff3c3d0a4cf0dcef79b9c98afcbaf3e98f4
Mek O <mek.arnuparp@gmail.com>
2020-01-03 16:27:49 -0800

    Fixed wrong download folder

--- coursespace/spiders/login.py
+++ coursespace/spiders/login.py
@@ -4,16 +4,13 @@ from getpass import *
 from scrapy.http import FormRequest
 from scrapy.http import Request
 from scrapy.shell import inspect_response
+import urllib.parse
 
 
 class LoginSpider(scrapy.Spider):
     name = 'coursespaces'
     allowed_domains = ['www.uvic.ca']
     start_urls = ['https://www.uvic.ca/cas/login?service=https%3A%2F%2Fcoursespaces.uvic.ca%2Flogin%2Findex.php']
-
-    custom_settings = {
-        'FILES_STORE': os.getcwd(),
-    }
 
     #UVIC CourseSpaces login
     def parse(self, response):
@@ -25,36 +22,31 @@ class LoginSpider(scrapy.Spider):
     #get links to each active courses
     def parse_coursespaces(self, response):
         # link = response.xpath("//*[@class=' row card-deck']/div[@class='card mb-3 courses-view-course-item']/a/@href").extract()
-        # - last 3 for irrelevent links
-        link = response.xpath("//*[@class=' row card-deck']/div[@class='card mb-3 courses-view-course-item']/a/@href").extract()
-        yield Request(link[0], callback=self.parse_file_list, dont_filter=True)
+        # link = response.xpath("//*[@class=' row card-deck']/div[@class='card mb-3 courses-view-course-item']/a/@href").extract()
+        # - last 3 links are irrevelent and shall be ignored
+        for course in response.xpath("//*[@class=' row card-deck']/div[@class='card mb-3 courses-view-course-item']/a/@href").extract()[0:-3]:
+            yield Request(course, callback=self.parse_file_list, dont_filter=True)
 
+    #prepare all links to each file for download
     def parse_file_list(self, response):
-        files = response.xpath("//div[@class='activityinstance']/a/@href").getall()
-        for f in files[1:5]:
+        folder_name = response.xpath("//div[@id='page-navbar']/nav/ol/li/a/@title").extract()[0]
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+        for f in response.xpath("//div[@class='activityinstance']/a/@href").getall():
             if('resource' in f):
-                yield Request(url=f, callback=self.get_file_link, dont_filter=True)
-        # print("\n\n\n\n\n\n\n%s\n\n\n\n\n\n" % f)
+                yield Request(url=f, callback=self.get_file_link,meta={'folder':folder_name}, dont_filter=True)
 
+    #prepare individual link for download
     def get_file_link(self, response):
         file_link = response.xpath("//div[@class='resourceworkaround']/a/@href").extract()
-        yield Request(url=file_link[0], callback=self.download, dont_filter=True)
-
+        yield Request(url=file_link[0], callback=self.download,meta={'folder':response.meta.get('folder')}, dont_filter=True)
 
     def download(self, response):
-        path = response.url.split('/')[-1]
-        self.logger.info('Saving PDF %s', path)
+        path = os.getcwd() + "/" + response.meta.get('folder')
+        filename = response.url.split('/')[-1]
+        filename = urllib.parse.unquote(filename).replace(" ", "_")
+        path = path + "/" + filename
+        self.logger.info('\n\n\tSaving PDF %s\n\n', path)
         with open(path, 'wb') as f:
             f.write(response.body)
-        # path = response.url.split('/')[-1]
-        # dirf = os.getcwd() + '/course'
-        # if not os.path.exists(dirf):os.makedirs(dirf)
-        # os.chdir(dirf)
-        # with open(path, 'wb') as f:
-        #     f.write(response.body)
-        # inspect_response(response,self)
-
-
-
-
 
